9fad3444a267cb6fa64f6d26a0a2804ffc0b40fd
Daniel-derf <danielpardinho321@gmail.com>
2024-12-05 08:27:44 -0300

    first unit test

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { InMemoryUsersRepository } from './repositories/users-repository-in-memory';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let usersDBRep: InMemoryUsersRepository;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
-    }).compile();
-
-    service = module.get<UsersService>(UsersService);
+    usersDBRep = new InMemoryUsersRepository();
+    service = new UsersService(usersDBRep);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a new user', async () => {
+    const newUser = await service.create({ name: 'Daniel', books: [] });
+    expect(await usersDBRep.findAll()).toEqual([newUser]);
   });
 });
