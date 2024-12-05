8235c512dd1a26d858fba472b84d80fecfd35ef0
Daniel-derf <danielpardinho321@gmail.com>
2024-12-05 08:40:07 -0300

    test: users crud

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -19,4 +19,35 @@ describe('UsersService', () => {
     const newUser = await service.create({ name: 'Daniel', books: [] });
     expect(await usersDBRep.findAll()).toEqual([newUser]);
   });
+
+  it('should delete a user', async () => {
+    await service.create({ name: 'Maria', books: [] });
+    await service.remove(1);
+
+    expect(await usersDBRep.findAll()).toEqual([]);
+  });
+
+  it('should edit a user', async () => {
+    await service.create({ name: 'Roberto', books: [] });
+
+    await service.update(1, { name: 'edited Roberto' });
+
+    expect(await usersDBRep.findById(1)).toEqual({
+      name: 'edited Roberto',
+      id: 1,
+      books: [],
+    });
+  });
+
+  it('should get a user by ID', async () => {
+    await service.create({ name: 'Roberto', books: [] });
+
+    const user = await service.findOne(1);
+
+    expect(user).toEqual({
+      name: 'Roberto',
+      id: 1,
+      books: [],
+    });
+  });
 });
